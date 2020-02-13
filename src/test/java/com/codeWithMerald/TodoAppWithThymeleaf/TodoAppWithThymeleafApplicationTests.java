d4e544361edb12715fff13871f6f8c7203ef5b1b
Meralds-World <grace.chidumaga@gmail.com>
2020-02-13 15:04:59 +0100

    create app with spring mvc

---
+++ src/test/java/com/codeWithMerald/TodoAppWithThymeleaf/TodoAppWithThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeWithMerald.TodoAppWithThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppWithThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
